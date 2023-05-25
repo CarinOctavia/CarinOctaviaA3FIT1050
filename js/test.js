22d9fece39b409cdd0ebd3620f97e392d82326ec
CarinOctavia <126581808+CarinOctavia@users.noreply.github.com>
2023-05-25 12:21:23 +1000

    Deploy Busy Bee Website
    A3 FIT1050 Completed

---
+++ js/test.js
@@ -0,0 +1,15 @@
+function showMessage(){
+    alert( 'JavaScript!' );
+    }
+document.querySelector('h1'). addEventListener('click', showMessage);
+
+Fancybox.bind("[data-fancybox]");
+
+$( 'button' ).click( filterItems );
+function filterItems() {
+    let button = this; 
+    let selector = $( button ).data( 'filter' );
+   console.log( selector );
+   $( '#products img' ).not (selector).hide(500);
+   $( selector ).show(500);
+}
